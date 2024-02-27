ef7ad3eb09c0aebe36dc65aec5b524b13b7280c7
farhanah110703 <farhanahsajidah11@gmail.com>
2024-02-27 13:13:20 +0700

    padding

--- structure.php
+++ structure.php
@@ -1,58 +1,63 @@
-
 <?php include "header.php"; ?>
-        <!-- ***** Breadcrumb Area Start ***** -->
-        <section class="section breadcrumb-area overlay-dark d-flex align-items-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <!-- Breamcrumb Content -->
-                        <div class="breadcrumb-content d-flex flex-column align-items-center text-center">
-                            <h2 class="text-white text-uppercase mb-3">Corporate Structure</h2>
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a class="text-uppercase text-white" href="index.html">Home</a></li>
+<!-- ***** Breadcrumb Area Start ***** -->
+<section class="section breadcrumb-area overlay-dark d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Breamcrumb Content -->
+                <div class="breadcrumb-content d-flex flex-column align-items-center text-center">
+                    <h2 class="text-white text-uppercase mb-3">Corporate Structure</h2>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a class="text-uppercase text-white" href="index.html">Home</a></li>
 
-                                <li class="breadcrumb-item text-white active">Corporate Structure</li>
-                            </ol>
-                        </div>
-                    </div>
+                        <li class="breadcrumb-item text-white active">Corporate Structure</li>
+                    </ol>
                 </div>
             </div>
-        </section>
-        <!-- ***** Breadcrumb Area End ***** -->
+        </div>
+    </div>
+</section>
+<!-- ***** Breadcrumb Area End ***** -->
 
-        <!-- ***** About Area Start ***** -->
-        <section class="section about-area ptb_100">
-        <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 129.4118%;">
-        <iframe src="https://docs.google.com/document/d/1HXCsm_u7ATa7NhUy-OAFi9TYfMI3qp6u9202GjRLfzA/preview?usp=embed_googleplus" 
-            style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen></iframe></div>
-        </section>
-        <!-- ***** About Area End ***** -->
+<!-- ***** About Area Start ***** -->
+<section class="section about-area ptb_100">
+    <div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 47.5%;">
+        <iframe
+            src="https://docs.google.com/document/d/1HXCsm_u7ATa7NhUy-OAFi9TYfMI3qp6u9202GjRLfzA/preview?usp=embed_googleplus"
+            style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen></iframe>
+    </div>
+</section>
+<!-- ***** About Area End ***** -->
 
 
-        <!-- ***** Our Goal Area End ***** -->
+<!-- ***** Our Goal Area End ***** -->
 
-        <!-- ***** Team Area Start ***** -->
+<!-- ***** Team Area Start ***** -->
 
-        <!-- ***** Team Area End ***** -->
+<!-- ***** Team Area End ***** -->
 
-        <!--====== Contact Area Start ======-->
-       
- 
+<!--====== Contact Area Start ======-->
 
-        <!--====== Call To Action Area Start ======-->
-        <section class="section cta-area bg-overlay ptb_100">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-12 col-lg-10">
-                        <!-- Section Heading -->
-                        <div class="section-heading text-center m-0">
-                            <h2 class="text-white"><?php print $enquiry_title; ?></h2>
-                            <p class="text-white d-none d-sm-block mt-4"><?php print $enquiry_text; ?></p>
-                            <a href="contact" class="btn btn-bordered-white mt-4">Contact Us</a>
-                        </div>
-                    </div>
+
+
+<!--====== Call To Action Area Start ======-->
+<section class="section cta-area bg-overlay ptb_100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <!-- Section Heading -->
+                <div class="section-heading text-center m-0">
+                    <h2 class="text-white">
+                        <?php print $enquiry_title; ?>
+                    </h2>
+                    <p class="text-white d-none d-sm-block mt-4">
+                        <?php print $enquiry_text; ?>
+                    </p>
+                    <a href="contact" class="btn btn-bordered-white mt-4">Contact Us</a>
                 </div>
             </div>
-        </section>
-        <!--====== Call To Action Area End ======-->
+        </div>
+    </div>
+</section>
+<!--====== Call To Action Area End ======-->
 <?php include "footer.php"; ?>
