f947a0ccf57aabc28a41f74e52e243fc833e06e3
amdryznn <amandamp24053@gmail.com>
2024-03-06 12:57:47 +0700

    show project in index

--- index.php
+++ index.php
@@ -185,7 +185,7 @@
         <div class="row items portfolio-items">
 
             <?php
-            $q = "SELECT * FROM  portfolio ORDER BY id DESC LIMIT 6";
+            $q = "SELECT * FROM  project ORDER BY id DESC LIMIT 6";
 
 
             $r123 = mysqli_query($con, $q);
@@ -193,24 +193,25 @@
             while ($ro = mysqli_fetch_array($r123)) {
 
                 $id = "$ro[id]";
-                $port_title = "$ro[port_title]";
-                $port_desc = "$ro[port_desc]";
+                $proj_title = "$ro[proj_title]";
+                $proj_desc = "$ro[proj_desc]";
                 $ufile = "$ro[ufile]";
+
 
                 print "
 <div class='col-12 col-sm-6 col-lg-4 portfolio-item' data-groups='['marketing','development']'>
 <!-- Single Case Studies -->
 <div class='single-case-studies'>
     <!-- Case Studies Thumb -->
-    <a href='portdetail.php?id=$id'>
-        <img src='dashboard/uploads/portfolio/$ufile' alt=''>
+    <a href='#'>
+        <img src='dashboard/uploads/project/$ufile' alt=''>
     </a>
     <!-- Case Studies Overlay -->
-    <a href='portdetail.php?id=$id' class='case-studies-overlay'>
+    <a href='#' class='case-studies-overlay'>
         <!-- Overlay Text -->
         <span class='overlay-text text-center p-3'>
-            <h3 class='text-white mb-3'>$port_title</h3>
-            <p class='text-white'>$port_desc</p>
+            <h3 class='text-white mb-3'>$proj_title</h3>
+            <p class='text-white'>$proj_desc.</p>
         </span>
     </a>
 </div>
